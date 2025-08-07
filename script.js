451ced436232f98e52e2cdcb5887ab195e395366
anamaria15082009 <maria.lima.ana1508@escola.pr.gov.br>
2025-08-07 20:51:10 -0300

    l

---
+++ script.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'QUEM SOU EU?',
+    'Meu nome é Ana,tenho 15 anos, sou carinhosa, responsavél, educada, caprichosa
+'
+)
+
+criaCartao(
+    'MEUS PETS?',
+    'Tenho uma cadelinha com o nome PITUCHA e um cachorro como nome URSO,tambem tenho um casal de calopsita.
+'
+)
+
+criaCartao(
+    'MEU PRESENTE?',
+    'Estudando e trabalhando'
+)
